e7d30406ef35f9764eb47693131a39f7b7e160cb
stargazer <already.late@gmail.com>
2011-04-21 21:50:11 +0200

    Created custom field lookup  that performs OR-separated, case-insensitive, comparison queries

--- piston_perfect/custom_filters.py
+++ piston_perfect/custom_filters.py
@@ -31,7 +31,40 @@ def in_all_filter(data, definition, values):
 	return data
 
 
+def isearch_filter(data, definition, values):
+	""" Handles the '__isearch' lookup filter, which performs case incensitive
+		search for all values in `values`, with an OR operator in between. 
+
+		For every value in tuple `values`, an `iexact` lookup filter is applied
+		and a Q query is created based on that:
+			Q = ( `field`__iexact = value),
+		where field is equal to definition[:-9]
+
+		All queries are applied one after the other, with an OR operator
+		joining their results.
+
+		@param data:		The dataset on which the queries will be applied on
+		@param definition:	A string representing <field> + '__isearch', eg
+							'recipients__isearch'
+		@param values:      Tuple with the values that will be applied on the
+							lookup filters.
+
+		@return:	        Remaining dataset after the filters have been
+							applied.
+	"""
+
+	field = definition[:-9]
+	query = Q()
+
+	for term in values:
+		query |= Q(**{ ('%s__iexact' % field): term })
+	return data.filter(query)
+
+	
+
+
 # Maps custom lookups to their handler methods
 filter_to_method = {
 	'__in_all': in_all_filter,
+	'__isearch': isearch_filter,
 }
