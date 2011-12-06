3e1fab71dae9377eb7e3fad0443779706c36a143
stargazer <already.late@gmail.com>
2011-12-06 11:24:45 +0100

    Optimized the implementation of __in_all filter

--- piston_perfect/custom_filters.py
+++ piston_perfect/custom_filters.py
@@ -12,23 +12,53 @@ def in_all_filter(data, definition, values):
 
 		@param data:		The dataset on which the filters will be applied on
 		@param definition:	A string representing <field> + '__in__all', eg
-							memberships__list__in_all
+							``memberships__list__in_all``
 		@param values:      Tuple with the values that will be applied on the
 							lookup filters.
 
 		@return:	        Remaining dataset after the filters have been
 							applied.
-	"""
-	field = definition[:-8]
-	
-	for value in values:
-		# If a value is empty, or is 'null', then we apply None as the field
-		# lookup value
-		if value in ['', 'null']:
-			value = None
 
-		data = data.filter(**{field:value})
-	return data
+		Example:
+		Query on the ContactHandler:
+		/contacts/?list=1&list=2&list=3
+		
+		The filter ``list`` is defined as``memberships_list__in_all``, so
+		method ``in_all_filter`` is called, with values=[1,2,3]. What the query asks for
+		is: give me all contacts that belong to all lists 1, 2 and 3.
+
+		So basically what we need to find is which ``Membership`` records contain a
+		``list_id`` with value either 1 or 2 or 3. Then we count the
+		appearances of Contact instances in this queryset. Every contact that
+		appears 3 times in the queryset, appears in all 3 lists.      
+
+		Note:
+		If any of the ``values`` is empty string, or `null` then it's the only
+		value we use to filter the data. Why? Well, say we performed a query to
+		get all contacts which belong to lists [2, 3, and null]. This means:
+		give me all contacts which belong to the list 2, list 3, and to no list
+		(...which is impossible of course).
+		So we just consider this as a query with poor semantics, and we return 
+		only contacts that belong to no list!
+
+	"""
+	#	eg. field = memberships__list__in                  
+	field_in = definition[:-4]	
+	#	eg. field = memberships_list
+	field_exact = definition[:-8]
+
+	# If an empty or ``null`` value is given, filter only based on None.
+	for value in values:
+		if value in ['', 'null']:
+			return data.filter(**{field_exact:None})
+
+	#	eg.	data.filter(memberships__contact__in=values)
+	query = Q(**{field_in:values})
+
+	return data.filter(query).\
+		annotate(count=Count('id')).\
+		filter(count=len(values))
+
 
 
 def isearch_filter(data, definition, values):
